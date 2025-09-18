0daa914ba459f640a6a2ebb991de4050ba449080
Vtrushch <106551682+Vtrushch@users.noreply.github.com>
2025-09-18 05:47:53 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+  // УВАГА: НЕ додавай output: 'export' (інакше API не працюватиме)
+};
+module.exports = nextConfig;
